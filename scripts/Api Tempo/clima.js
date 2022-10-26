2970d4f7bc8f1e689473aee7e049308ed82d94db
Edilson-EJ <edilsonjunior624@gmail.com>
2022-10-26 00:35:04 -0300

    news

--- scripts/Api Tempo/clima.js
+++ scripts/Api Tempo/clima.js
@@ -1,13 +1,13 @@
 const api = {
-    key: 'd1d26429eea748575902a889420ce638',
-    base: 'http://api.openweathermap.org/data/3.0/',
-    lang: 'pt-br',
-    units: 'metric',
-    city: 'fortaleza'
+    key: "d1d26429eea748575902a889420ce638",
+    base: "http://api.openweathermap.org/data/3.0/",
+    lang: "pt-br",
+    units: "metric",
+    city: "fortaleza"
 }
 
 
-fetch(`${api.base}weather?q=${api.city}&lang${api.lang}&units=${api.units}&appid=${api.key}`
+fetch(`http://api.openweathermap.org/data/2.5/weather?q=fortaleza&langpt-b&units=metric&appid=d1d26429eea748575902a889420ce638`
 ).then((response) =>{
     return response.json();
 }).then((jsonParsed) =>{

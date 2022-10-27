70a6b25856bfb505c3ae394268d81ae6da3266a2
Edilson-EJ <edilsonjunior624@gmail.com>
2022-10-26 21:11:20 -0300

    news

--- scripts/Api Tempo/clima.js
+++ scripts/Api Tempo/clima.js
@@ -12,4 +12,28 @@ fetch(`http://api.openweathermap.org/data/2.5/weather?q=fortaleza&langpt-b&units
     return response.json();
 }).then((jsonParsed) =>{
     console.log(jsonParsed)
+
+    jsonParsed.main.forEach(element =>{
+        const temp_atual = element.temp 
+        const temperatura_max = element.temp_max
+        const temperatura_min = element.temp_min 
+        const humidade = element.humidity 
+
+        const temperatura_atual = document.getElementById('temp_atual');
+        temperatura_atual.innerText = temp_atual + '°'
+
+        const temperatura_mx = document.getElementById('temp_max');
+        temperatura_mx.innerText = temperatura_max + '°'
+
+        const temperatura_mn = document.getElementById('temp_min');
+        temperatura_mn.innerText = temperatura_min+ '°'
+
+        const humidade_temp = document.getElementById('humidade');
+        humidade_temp.innerText = humidade + '%'
+    })
+
 })
+
+
+
+        
